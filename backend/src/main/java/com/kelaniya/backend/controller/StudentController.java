d3e5af965fee8fe5cafa50737615b02e657eecff
MalshaniDahanayaka <mekaladahanayaka80@gmail.com>
2022-04-18 06:44:34 +0530

    add profile photo uploading controller

--- backend/src/main/java/com/kelaniya/backend/controller/StudentController.java
+++ backend/src/main/java/com/kelaniya/backend/controller/StudentController.java
@@ -3,12 +3,17 @@ package com.kelaniya.backend.controller;
 import com.kelaniya.backend.entity.Students;
 import com.kelaniya.backend.entity.Users;
 import com.kelaniya.backend.service.StudentService;
+import com.kelaniya.backend.service.imgServices.ImageUploadResponse;
+import com.kelaniya.backend.utils.ImgFileUploadUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 public class StudentController {
@@ -32,4 +37,24 @@ public class StudentController {
   public Students updateDetails(@RequestBody Students student){
       return studentService.updateDetails(student);
   }
+
+  @PostMapping("/api/v1/student/update-details/uploadImageFile")
+  public ResponseEntity<ImageUploadResponse> uploadFile(
+          @RequestParam("file") MultipartFile multipartFile)
+          throws IOException {
+
+    String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+    long size = multipartFile.getSize();
+
+    String filecode = ImgFileUploadUtil.saveImage(fileName, multipartFile);
+
+    ImageUploadResponse response = new ImageUploadResponse();
+    response.setFileName(fileName);
+    response.setSize(size);
+    response.setImgUploadUrl("/downloadFile/" + filecode);
+
+    return new ResponseEntity<>(response, HttpStatus.OK);
+  }
+
+
 }
