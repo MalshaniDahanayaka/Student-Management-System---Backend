925a56a44cced6c1aa42dc3a85c60f687cfd7f76
MalshaniDahanayaka <mekaladahanayaka80@gmail.com>
2022-04-18 06:47:07 +0530

    add image file upload util class

---
+++ backend/src/main/java/com/kelaniya/backend/utils/ImgFileUploadUtil.java
@@ -0,0 +1,33 @@
+package com.kelaniya.backend.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+public class ImgFileUploadUtil {
+    public static String saveImage(String fileName, MultipartFile multipartFile)
+            throws IOException {
+        Path uploadPath = Paths.get("D:\\db_data\\profile_Images");
+
+        if (!Files.exists(uploadPath)) {
+            Files.createDirectories(uploadPath);
+        }
+
+        String fileCode = RandomStringUtils.randomAlphanumeric(8);
+
+        try (InputStream inputStream = multipartFile.getInputStream()) {
+            Path filePath = uploadPath.resolve(fileCode + "-" + fileName);
+            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException ioe) {
+            throw new IOException("Could not save file: " + fileName, ioe);
+        }
+
+        return fileCode;
+    }
+}
