cc1d4c8d76c6386fce490638073009362a375b58
MalshaniDahanayaka <mekaladahanayaka80@gmail.com>
2022-04-19 16:51:46 +0530

    add LectureNotesService class with saveFile and get file by id methods

---
+++ backend/src/main/java/com/kelaniya/backend/service/LectureNotesService.java
@@ -0,0 +1,37 @@
+package com.kelaniya.backend.service;
+
+import com.kelaniya.backend.entity.LecNotes;
+import com.kelaniya.backend.repository.LecNoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class LectureNotesService {
+    @Autowired
+    private LecNoteRepository lecNoteRepository;
+
+
+    public LecNotes saveFile(MultipartFile file,String subjectID,String description) {
+        String docname = file.getOriginalFilename();
+        try {
+            LecNotes doc = new LecNotes(subjectID,description,file.getBytes(),file.getContentType());
+            return lecNoteRepository.save(doc);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    public Optional<LecNotes> getFile(Integer fileId) {
+        return lecNoteRepository.findById(fileId);
+    }
+    public List<LecNotes> getFiles(){
+        return lecNoteRepository.findAll();
+    }
+}
