9552e8a137734ac27d40af50eed59246b17c7180
MalshaniDahanayaka <mekaladahanayaka80@gmail.com>
2022-04-19 16:47:43 +0530

    update studentController with getLectureNotes getMapping

--- backend/src/main/java/com/kelaniya/backend/controller/StudentController.java
+++ backend/src/main/java/com/kelaniya/backend/controller/StudentController.java
@@ -1,12 +1,18 @@
 package com.kelaniya.backend.controller;
 
+import com.kelaniya.backend.entity.LecNotes;
 import com.kelaniya.backend.entity.Students;
 import com.kelaniya.backend.entity.Users;
+import com.kelaniya.backend.repository.LecNoteRepository;
+import com.kelaniya.backend.service.LectureNotesService;
 import com.kelaniya.backend.service.StudentService;
 import com.kelaniya.backend.service.imgServices.ImageUploadResponse;
 import com.kelaniya.backend.utils.ImgFileUploadUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.util.StringUtils;
@@ -14,12 +20,19 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 public class StudentController {
 
   @Autowired
   private StudentService studentService;
+
+  @Autowired
+  private LecNoteRepository lecNoteRepository;
+
+  @Autowired
+  private LectureNotesService lectureNotesService;
 
 
   @GetMapping("/api/v1/student")
@@ -38,23 +51,46 @@ public class StudentController {
       return studentService.updateDetails(student);
   }
 
+
+
   @PostMapping("/api/v1/student/update-details/uploadImageFile")
-  public ResponseEntity<ImageUploadResponse> uploadFile(
-          @RequestParam("file") MultipartFile multipartFile)
-          throws IOException {
+  public ResponseEntity<ImageUploadResponse> uploadFile(@RequestParam("file") MultipartFile multipartFile,
+                                                        @RequestParam("description") String description) throws IOException {
+    System.out.println(description);
 
     String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+    System.out.println(fileName);
     long size = multipartFile.getSize();
 
-    String filecode = ImgFileUploadUtil.saveImage(fileName, multipartFile);
+    String fileCode = ImgFileUploadUtil.saveImage(fileName, multipartFile);
+
 
     ImageUploadResponse response = new ImageUploadResponse();
     response.setFileName(fileName);
     response.setSize(size);
-    response.setImgUploadUrl("/downloadFile/" + filecode);
+    response.setImgUploadUrl("/downloadFile/" + fileCode);
 
     return new ResponseEntity<>(response, HttpStatus.OK);
   }
+
+
+  @GetMapping("/api/v1/docs")
+  public List<LecNotes> getNotes() {
+
+    return lecNoteRepository.findAll();
+  }
+
+
+      @GetMapping("/downloadFile/{fileId}")
+    public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable Integer fileId){
+        LecNotes doc = lectureNotesService.getFile(fileId).get();
+        System.out.println(doc.getSubjectName());
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(doc.getDocType()))
+                .header(HttpHeaders.CONTENT_DISPOSITION,"attachment:filename=\""+doc.getSubjectName()+"\"")
+                .body(new ByteArrayResource(doc.getData()));
+    }
 
 
 }
