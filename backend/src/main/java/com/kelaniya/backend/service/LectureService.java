04362b0c75522d50550306fe1e64ccf774277bd0
MalshaniDahanayaka <mekaladahanayaka80@gmail.com>
2022-04-20 11:38:04 +0530

    add two methods for lectureService class(addNewCourse() and deleteCourseModule() )

--- backend/src/main/java/com/kelaniya/backend/service/LectureService.java
+++ backend/src/main/java/com/kelaniya/backend/service/LectureService.java
@@ -1,6 +1,8 @@
 package com.kelaniya.backend.service;
 
+import com.kelaniya.backend.entity.Courses;
 import com.kelaniya.backend.entity.LecNotes;
+import com.kelaniya.backend.repository.CourseRepository;
 import com.kelaniya.backend.repository.LecNoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,11 +12,15 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class LectureNotesService {
+public class LectureService {
     @Autowired
     private LecNoteRepository lecNoteRepository;
 
+    @Autowired
+    private CourseRepository courseRepository;
 
+
+    //Save uploaded file
     public LecNotes saveFile(MultipartFile file,String subjectID,String description) {
         String docname = file.getOriginalFilename();
         try {
@@ -28,11 +34,33 @@ public class LectureNotesService {
     }
 
 
+    //get selected file
     public Optional<LecNotes> getFile(Integer fileId) {
 
         return lecNoteRepository.findById(fileId);
     }
+
+    //get all uploaded files
     public List<LecNotes> getFiles(){
         return lecNoteRepository.findAll();
+    }
+
+    //add new subject
+    public Courses addNewCourse(String courseID,String courseName,String  lectureEmail){
+        try {
+            Courses course = new Courses(courseID,courseName,lectureEmail);
+            return courseRepository.save(course);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    //delete subject
+    public int deleteCourseModule(String courseID){
+
+        return courseRepository.deleteSelectedCourseModule(courseID);
     }
 }
