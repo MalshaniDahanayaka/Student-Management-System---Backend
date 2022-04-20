99091227e6b8074cde0a1fbf4578455883caf737
MalshaniDahanayaka <mekaladahanayaka80@gmail.com>
2022-04-20 13:59:47 +0530

    create 3 methods for add students marks and grades, update student marks and grade and create new announcement

--- backend/src/main/java/com/kelaniya/backend/controller/LecturerFeaturesController.java
+++ backend/src/main/java/com/kelaniya/backend/controller/LecturerFeaturesController.java
@@ -40,6 +40,13 @@ public class LecturerFeaturesController {
     }
 
 
+
+
+
+
+
+
+
     //create new course
     @PostMapping("/lecture/new_course/")
     public String addNewCourseModule(@RequestParam("course_id") String courseID,@RequestParam("course_name") String courseName,
@@ -51,6 +58,11 @@ public class LecturerFeaturesController {
     }
 
 
+
+
+
+
+
     //delete course module
     @DeleteMapping("/drop/{courseID}")
     public String removeCourseModule(@PathVariable String courseID){
@@ -59,6 +71,53 @@ public class LecturerFeaturesController {
 
            return "/drop/{courseID}";
     }
+
+
+
+
+
+
+
+
+    //add marks and grades for students
+    @PostMapping("/add/marks_and_grades/")
+    public String addMarksAndGrades(@RequestParam("studentEmail") String student_email, @RequestParam("courseID") String course_id,
+                                    @RequestParam("score") double score, @RequestParam("grade") String grade){
+        lectureService.addMarksAndGrades(student_email,course_id,score,grade);
+
+        return "/add/marks_and_grade/";
+    }
+
+
+
+
+
+
+
+
+    //update marks and grades
+    @PatchMapping("/update/marks_and_grades/")
+    public String updateMarksAndGrades(@RequestParam("studentEmail") String student_email, @RequestParam("courseID") String course_id,
+                                       @RequestParam("score") double score, @RequestParam("grade") String grade){
+        lectureService.updateMarksAndGrades(student_email,course_id,score,grade);
+        return "/update/marks_and_grades/";
+
+    }
+
+
+
+
+
+    //create Announcement
+    @PostMapping("/lec/Anouncement/")
+    public String makeAnnouncement(@RequestParam("lecturer_email") String lecturer_email,@RequestParam("title") String title,
+                                   @RequestParam("body") String body,@RequestParam("category") String category){
+        lectureService.addAnnouncement(lecturer_email,title,body,category);
+
+        return "/lec/Anouncement/";
+    }
+
+
 
 
 }
