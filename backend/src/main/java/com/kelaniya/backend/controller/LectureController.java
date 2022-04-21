e2f0234793ca4f388a5cbd03bcd54f314d08c0fb
Thushara Samaraweera <thusharadasun204@gmail.com>
2022-04-21 14:22:33 +0530

    add lecturer signup

--- backend/src/main/java/com/kelaniya/backend/controller/LectureController.java
+++ backend/src/main/java/com/kelaniya/backend/controller/LectureController.java
@@ -1,18 +1,30 @@
 package com.kelaniya.backend.controller;
 
 import com.kelaniya.backend.entity.Lecturers;
+import com.kelaniya.backend.entity.Users;
+import com.kelaniya.backend.service.LecturerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LectureController {
 
+  @Autowired
+  private LecturerService lecturerService;
+
   @GetMapping("/api/v1/lecturer")
   @PreAuthorize("hasRole('Lecture')")
   public String lecture(){
     return "For Lecturer";
+  }
+
+  @PostMapping("api/v1/auth/signup-lecturer")
+  public Users addUser(@RequestBody Users users){
+    return lecturerService.signup(users);
   }
 
 }
--- backend/src/main/java/com/kelaniya/backend/service/LecturerService.java
+++ backend/src/main/java/com/kelaniya/backend/service/LecturerService.java
@@ -1,9 +1,37 @@
 package com.kelaniya.backend.service;
 
+import com.kelaniya.backend.entity.Lecturers;
+import com.kelaniya.backend.entity.Role;
+import com.kelaniya.backend.entity.Users;
+import com.kelaniya.backend.repository.RoleRepository;
+import com.kelaniya.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class LecturerService {
 
+  @Autowired
+  private RoleRepository roleRepository;
 
+  @Autowired
+  private UserService userService;
+
+  @Autowired
+  private UserRepository userRepository;
+
+
+  public Users signup(Users users){
+    Role lectureRole = roleRepository.findById("Lecturer").get();
+
+    Set<Role> roles = new HashSet<>();
+    roles.add(lectureRole);
+    users.setRole(roles);
+
+    users.setPassword(userService.getEncodePassword((users.getPassword())));;
+    return userRepository.save(users);
+  }
 }
