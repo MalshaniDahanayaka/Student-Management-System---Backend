9981449b5907efc4f4c25a2abe5eb5e3569d911d
MalshaniDahanayaka <mekaladahanayaka80@gmail.com>
2022-05-03 10:18:51 +0530

    git add course delete request class

---
+++ backend/src/main/java/com/kelaniya/backend/entity/request/CourseDeleteRequest.java
@@ -0,0 +1,24 @@
+package com.kelaniya.backend.entity.request;
+
+public class CourseDeleteRequest {
+
+    private String course_id;
+    private String academic_year;
+
+
+    public String getCourse_id() {
+        return course_id;
+    }
+
+    public void setCourse_id(String course_id) {
+        this.course_id = course_id;
+    }
+
+    public String getAcademic_year() {
+        return academic_year;
+    }
+
+    public void setAcademic_year(String academic_year) {
+        this.academic_year = academic_year;
+    }
+}
--- backend/src/main/java/com/kelaniya/backend/utils/JwtUtil.java
+++ backend/src/main/java/com/kelaniya/backend/utils/JwtUtil.java
@@ -16,7 +16,7 @@ public class JwtUtil {
 
     private static final String SECRET_KEY = "secret";
 
-    private static final int TOKEN_VALIDITY = 3600 * 5;
+    private static final int TOKEN_VALIDITY = 3600 * 24;
 
     public String getUsernameFromToken(String token) {
         return getClaimFromToken(token, Claims::getSubject);
