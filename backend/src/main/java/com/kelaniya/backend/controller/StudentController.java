8af8a66dc1d223295f72c2fb750953c6a1bdaedf
MalshaniDahanayaka <mekaladahanayaka80@gmail.com>
2022-05-03 10:06:59 +0530

    update student controller class with student details find method

--- backend/src/main/java/com/kelaniya/backend/controller/StudentController.java
+++ backend/src/main/java/com/kelaniya/backend/controller/StudentController.java
@@ -1,26 +1,20 @@
 package com.kelaniya.backend.controller;
 
-import com.kelaniya.backend.configuration.auth.JwtRequestFilter;
-import com.kelaniya.backend.entity.LecNotes;
-import com.kelaniya.backend.entity.Students;
-import com.kelaniya.backend.entity.Users;
+import com.kelaniya.backend.entity.*;
+import com.kelaniya.backend.repository.CourseRepository;
 import com.kelaniya.backend.repository.LecNoteRepository;
+import com.kelaniya.backend.repository.UserRoleRepository;
 import com.kelaniya.backend.service.LectureService;
 import com.kelaniya.backend.service.StudentService;
 import com.kelaniya.backend.service.imgServices.ImageUploadResponse;
 import com.kelaniya.backend.utils.ImgFileUploadUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,9 +22,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -44,7 +39,15 @@ public class StudentController {
   private LecNoteRepository lecNoteRepository;
 
   @Autowired
+  private CourseRepository courseRepository;
+
+  @Autowired
   private LectureService lectureService;
+
+  @Autowired
+  private UserRoleRepository userRoleRepository;
+
+
 
 
   @GetMapping("/api/v1/student")
@@ -62,10 +65,61 @@ public class StudentController {
 
 
 
+
   @PostMapping("/api/v1/student/update-details")
   public Students updateDetails(@RequestBody Students student){
       return studentService.updateDetails(student);
   }
+
+
+
+  //get selected Course details
+  @GetMapping("/api/v1/Courses/{courseID}")
+  public Courses getSelectedCourse(@PathVariable String courseID ){
+    return studentService.getSelectedCourseDetails(courseID);
+  }
+
+
+
+ // get enrolled courses list
+  @GetMapping("/api/v1/Courses/enrolledCourses")
+  public List<StudentsEnrollSubjects> getEnrolledCourses(HttpServletRequest request){
+    HttpSession session = request.getSession();
+    String userEmail = (String) session.getAttribute("userEmail");
+
+    return studentService.getEnrollCourses(userEmail);
+  }
+
+
+  //get department Courses list
+  @GetMapping("/api/v1/Courses/department_courses/{department}")
+  public List<Courses> getDepartmentCourses(@PathVariable String department){
+
+      return studentService.getUserDepartmentCourseModules(department);
+  }
+
+
+
+ // @get all Courses list
+  @GetMapping("/api/v1/Courses/all")
+  public List<Courses> getAllCourses(){
+      return studentService.getAllCourses();
+  }
+
+
+
+
+
+
+
+
+
+  @GetMapping("/api/v1/docs/{id}")
+  public Optional<LecNotes> getNote(@PathVariable String id) {
+
+    return lecNoteRepository.findById(id);
+  }
+
 
 
 
@@ -89,33 +143,6 @@ public class StudentController {
     return new ResponseEntity<>(response, HttpStatus.OK);
   }
 
-
-  @GetMapping("/api/v1/docs")
-  public List<LecNotes> getNotes(HttpServletRequest request, HttpServletResponse response) {
-    HttpSession session = request.getSession();
-    String userEmail = (String) session.getAttribute("userEmail");
-    //System.out.println(userEmail);
-    return lecNoteRepository.findAll();
-  }
-
-
-  @GetMapping("/api/v1/docs/{id}")
-  public Optional<LecNotes> getNote(@PathVariable String id) {
-
-    return lecNoteRepository.findById(id);
-  }
-
-
-
-
-//    @GetMapping("/downloadFile/{fileId}")
-//    public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable Integer fileId){
-//      LecNotes doc = lectureService.getFile(fileId).get();
-//
-//      return ResponseEntity.ok()
-//              .header(HttpHeaders.CONTENT_DISPOSITION,"attachment:filename=")
-//              .body(new ByteArrayResource(doc.getData()));
-//    }
 
 
 }
