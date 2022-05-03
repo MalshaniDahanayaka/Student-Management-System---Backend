aa0b5d741353900b6bcabacaeb5216ef21f9c554
MalshaniDahanayaka <mekaladahanayaka80@gmail.com>
2022-05-03 10:11:59 +0530

    add cutom queries to get student details

--- backend/src/main/java/com/kelaniya/backend/repository/StudentRepository.java
+++ backend/src/main/java/com/kelaniya/backend/repository/StudentRepository.java
@@ -1,9 +1,17 @@
 package com.kelaniya.backend.repository;
 
 import com.kelaniya.backend.entity.Students;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
 @Repository
-public interface StudentRepository extends CrudRepository<Students, String> {
+public interface StudentRepository extends JpaRepository<Students, String> {
+
+    @Transactional
+    @Query("FROM students where student_email = ?1")
+    Students getUserStudentDetails(String studentEmail);
+
 }
