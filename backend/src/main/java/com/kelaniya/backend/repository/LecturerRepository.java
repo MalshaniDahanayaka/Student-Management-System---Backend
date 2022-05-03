2bea23df3279fdd409ecbe3462ff11dc8189c839
MalshaniDahanayaka <mekaladahanayaka80@gmail.com>
2022-05-03 10:23:28 +0530

    add lecture repository class to the data retriew and insert data to lecturer table

---
+++ backend/src/main/java/com/kelaniya/backend/repository/LecturerRepository.java
@@ -0,0 +1,13 @@
+package com.kelaniya.backend.repository;
+
+import com.kelaniya.backend.entity.Lecturers;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.transaction.Transactional;
+
+public interface LecturerRepository extends JpaRepository<Lecturers,String> {
+    @Transactional
+    @Query("FROM lectures where lecturer_email = ?1")
+    Lecturers getUserLecturerDetails(String lectureEmail);
+}
