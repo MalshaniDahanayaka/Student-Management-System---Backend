6785e16f57d01a0c83e320bae266b8da147de19e
Thushara Samaraweera <thusharadasun204@gmail.com>
2022-05-13 09:48:08 +0530

    add get all faculties endpoint

--- backend/src/main/java/com/kelaniya/backend/controller/PublicController.java
+++ backend/src/main/java/com/kelaniya/backend/controller/PublicController.java
@@ -1,12 +1,26 @@
 package com.kelaniya.backend.controller;
 
+import com.kelaniya.backend.entity.Faculty;
+import com.kelaniya.backend.service.PublicService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class PublicController {
+
+  @Autowired
+  private PublicService publicService;
+
   @GetMapping("/api/v1/public")
   private String forPublic(){
     return "public";
+  }
+
+  @GetMapping("/api/v1/public/faculty")
+  private List<Faculty> getAllFaculty(){
+    return publicService.getAllFaculty();
   }
 }
