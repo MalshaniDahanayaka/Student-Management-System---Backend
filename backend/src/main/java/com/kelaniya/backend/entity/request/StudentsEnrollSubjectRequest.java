d3dc11818d10538a80a6646e5a1af9e9fe340d2d
Radhushani <radushani111@gmail.com>
2022-05-17 12:41:58 +0530

    new request entity added

--- backend/src/main/java/com/kelaniya/backend/entity/request/StudentsEnrollSubjectRequest.java
+++ backend/src/main/java/com/kelaniya/backend/entity/request/StudentsEnrollSubjectRequest.java
@@ -1,2 +1,23 @@
-package com.kelaniya.backend.entity.request;public class StudentsEnrollSubjectRequest {
+package com.kelaniya.backend.entity.request;
+
+public class StudentsEnrollSubjectRequest {
+
+    private String enrolled_course_id;
+    private String academic_year;
+
+    public String getEnrolled_course_id() {
+        return enrolled_course_id;
+    }
+
+    public void setEnrolled_course_id(String enrolled_course_id) {
+        this.enrolled_course_id = enrolled_course_id;
+    }
+
+    public String getAcademic_year() {
+        return academic_year;
+    }
+
+    public void setAcademic_year(String academic_year) {
+        this.academic_year = academic_year;
+    }
 }
